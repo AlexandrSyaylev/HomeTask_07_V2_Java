aecea786357a40d039e2da3e51dd75e5179b7b44
Alexander Syayalev <alexxs@inbox.ru>
2022-02-08 20:09:47 +0300

    refoemat code, check all variables&methods name

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -14,13 +14,15 @@ public class Radio {
     public void prevStation() {
         if (currentStation == 0) {
             currentStation = 9;
-        } else {currentStation = currentStation - 1;}
+        } else {
+            currentStation = currentStation - 1;
+        }
     }
 
     public void setCurrentStation(int currentStation) {
         if (currentStation >= 0 & currentStation < 10) {
-                this.currentStation = currentStation;
-            }
+            this.currentStation = currentStation;
+        }
     }
 
     public int getCurrentStation() {
@@ -39,10 +41,11 @@ public class Radio {
         }
     }
 
-    public int getCurrentVolume () {
+    public int getCurrentVolume() {
         return currentVolume;
     }
-    public void setCurrentVolume (int currentVolume){
+
+    public void setCurrentVolume(int currentVolume) {
         if (currentVolume >= 0 & currentVolume <= 10) {
             this.currentVolume = currentVolume;
         }
